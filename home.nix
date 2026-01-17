b47f6b57cce685a3f1704fe3c191292b4990faad
Muddyf0x <fuchs.lars@protonmail.com>
2026-01-17 17:20:00 +0100

    initial commit

---
+++ home.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+let 
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  configs = {
+    nvim = "nvim";
+    alacritty = "alacritty";
+  };
+in
+{
+  home.username = "muddy";
+  home.homeDirectory = "/home/muddy";
+  programs.git.enable = true;
+  home.stateVersion = "25.11";
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      neofetch = "fastfetch";
+    };
+  };
+
+  xdg.configFile = builtins.mapAttrs (name: subpath: {
+    source = create_symlink "${dotfiles}/${subpath}";
+    recursive = true;
+  }) configs;
+
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+    fastfetch
+  ];
+}
